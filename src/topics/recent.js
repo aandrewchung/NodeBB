dc9591273b856d575bc159241d9c7f7b02457bec
Andrew Chung <aandrewchung@gmail.com>
2023-09-06 17:24:50 -0400

    reset code n setting up interface

--- src/topics/recent.js
+++ src/topics/recent.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function setupTopics(Topics) {
-    const terms = {
-        day: 86400000,
-        week: 604800000,
-        month: 2592000000,
-        year: 31104000000,
-    };
-    // No changes made to the code inside the function.
-}
-exports.default = setupTopics;
+const terms = {
+    day: 86400000,
+    week: 604800000,
+    month: 2592000000,
+    year: 31104000000,
+};
+const Topics = {};
+exports.default = Topics;
--- src/topics/recent.ts
+++ src/topics/recent.ts
@@ -1,21 +1,16 @@
 import db from '../database';
-import plugins from '../plugins';
-import posts from '../posts';
 
-interface Terms {
-    day: number;
-    week: number;
-    month: number;
-    year: number;
+interface TopicsInterface {
 }
 
-export default function setupTopics(Topics: any): void {
-    const terms: Terms = {
-        day: 86400000,
-        week: 604800000,
-        month: 2592000000,
-        year: 31104000000,
-    };
+const terms = {
+    day: 86400000,
+    week: 604800000,
+    month: 2592000000,
+    year: 31104000000,
+};
 
-    // No changes made to the code inside the function.
-}
+const Topics: TopicsInterface = {
+};
+
+export default Topics;
